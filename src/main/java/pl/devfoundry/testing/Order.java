c29f16fce3ef43db9d7dbf20ef43f7d845c2b512
Michal Koperski <mkoperpno@gmali.com>
2020-01-19 14:54:51 +0100

    Add new Class OrderBackup and OrderBackupTEst

--- src/main/java/pl/devfoundry/testing/Order.java
+++ src/main/java/pl/devfoundry/testing/Order.java
@@ -22,6 +22,13 @@ public class Order {
     void cancel() {
         this.meals.clear();
     }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "meals=" + meals +
+                '}';
+    }
 }
 
 
---
+++ src/test/java/pl/devfoundry/testing/OrderBackupTEst.java
@@ -0,0 +1,39 @@
+package pl.devfoundry.testing;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class OrderBackupTEst {
+
+    private static OrderBackup orderBackup;
+
+    //BeforeAll jest przed BefaorEach
+    @BeforeAll //metody pod ta adnotacja musza byc statyczne
+    static void setup() throws IOException {
+        orderBackup=new OrderBackup();
+        orderBackup.createFile();
+    }
+
+    @Test
+    void backupOrderWithOneMeal() throws IOException {
+        //given
+        Meal meal = new Meal(7,"Fries");
+        Order order = new Order();
+        order.addMealToOrder(meal);
+
+        //when
+        orderBackup.backupOrder(order);
+
+        //then
+        System.out.println("Order: "+order.toString()+" backed up.");
+
+    }
+    @AfterAll //AfterAll jest po AfterEach
+    static void tearDown() throws IOException {
+        orderBackup.closeFile();
+    }
+
+}
